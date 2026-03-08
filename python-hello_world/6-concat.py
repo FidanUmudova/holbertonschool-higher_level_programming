e7c09e469b7e383bbd26a940918d9f28de24674f
FidanUmudova <fidanumudova05@gmail.com>
2026-03-08 06:54:28 +0000

    Fix 6-concat.py to use str1 and str2 in welcome message

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1= "Holberton"
 str2 = "School"
-# Print the full welcome message using atr1 and str2
+# Print the welcome message using str1 and str2
 print(f"Welcome to {str1} {str2}!")
