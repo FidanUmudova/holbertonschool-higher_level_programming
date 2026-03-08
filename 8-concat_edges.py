d5ddc56e37977ad818e915139101cf29a34bd6c6
FidanUmudova <fidanumudova05@gmail.com>
2026-03-08 13:28:14 +0000

    Fix 8-concat_edges.py to print required string using slicing only

---
+++ 8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Python is an interpretend, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+# print the exact required string using slicing only 
+print(str[40:67] + " with " + str[:6])
