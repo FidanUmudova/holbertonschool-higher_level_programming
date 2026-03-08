cc4f656a9ecf9e6948b4d945ef79936c48a23ace
FidanUmudova <fidanumudova05@gmail.com>
2026-03-08 07:44:06 +0000

    Add slicing to print first 3, last 2 and middle of word

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-# get first 3 letters
 word_first_3 = word[:3]
-# get last 2 letters
 word_last_2 = word[-2:]
-# get middle of the word (without first and last letters)
 middle_word = word[1:-1]
-# print results
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
