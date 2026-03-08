8091f531f4c30d1f40a32cad19ec7620d04a1535
FidanUmudova <fidanumudova05@gmail.com>
2026-03-08 14:23:28 +0000

    Fix 8-concat_edges.py to use slicing only with str

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Python is an interpreted, interactive, object-oriented programming\ language that combines remarkable power with very clear syntax"
+str = "Python is an interpreted, interactive, object-oriented programming\ language that combines remarkable power with very clear syntax"
 # print the exact required string using slicing only 
-print(str1[39:66] + str1[107:113] + str1[:6])
+print(str[39:66] + str[107:113] + str[:6])
 
