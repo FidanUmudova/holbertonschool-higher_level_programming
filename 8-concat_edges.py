30a84cb33379ba11ff6387027598aa57789db695
FidanUmudova <fidanumudova05@gmail.com>
2026-03-09 12:10:11 +0000

    Add 2-print.py and remove 8-concat_edges.py

--- 8-concat_edges.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-str = "Python is an interpretend, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-# print the exact required string using slicing only 
-print(str[40:67] + " with " + str[:6])
