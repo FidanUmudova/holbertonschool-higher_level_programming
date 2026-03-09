316cae075e72a14ae5db0a721cd3e05be9264205
FidanUmudova <fidanumudova05@gmail.com>
2026-03-09 11:14:22 +0000

    Add 8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,8 @@
 def uppercase(str):
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
-            print("{}".format(chr(ord(c) - 32)), end="")
+            print("{:c}".format(ord(c) - 32), end="")
         else:
-            print("{}".format(c), end="")
+            print("{:c}".format(ord(c)), end="")
     print("")
 
