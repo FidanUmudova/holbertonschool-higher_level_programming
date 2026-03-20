d5bd8df942a1cb08419b36c7d8494dbd340574e5
Fidan Umudova <fidanumudova05@gmail.com>
2026-03-20 13:16:13 +0400

    Enhance type checks in add_integer function
    Added NaN check for parameters a and b in add_integer function.

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,16 +5,10 @@
 def add_integer(a, b=98):
     """Adds two integers and returns the result"""
 
-    if not isinstance(a, (int, float)):
+    if not isinstance(a, (int, float)) or a != a:  
         raise TypeError("a must be an integer")
 
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
-
-    # NaN check
-    if a != a:
-        raise TypeError("a must be an integer")
-    if b != b:
+    if not isinstance(b, (int, float)) or b != b:  
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
