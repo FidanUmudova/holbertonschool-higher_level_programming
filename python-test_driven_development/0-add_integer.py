e63600a6e7bbe7897e12826b4b661b7d4fd0ba05
Fidan Umudova <fidanumudova05@gmail.com>
2026-03-20 13:24:38 +0400

    Enhance type checks for add_integer function

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,14 +5,16 @@
 def add_integer(a, b=98):
     """Adds two integers and returns the result"""
 
+    # Check type
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-        
-    if isinstance(a, float) and (a != a or a in (float('inf'), float('-inf'))):
+
+    # Check for NaN or Infinity
+    if isinstance(a, float) and (a != a or a == float('inf') or a == float('-inf')):
         raise TypeError("a must be an integer")
-    if isinstance(b, float) and (b != b or b in (float('inf'), float('-inf'))):
+    if isinstance(b, float) and (b != b or b == float('inf') or b == float('-inf')):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
