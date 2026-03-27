3413b4dba7a0fb25ad5008fa9841f5f1b70a9982
Fidan Umudova <fidanumudova05@gmail.com>
2026-03-27 13:39:22 +0400

    Create task_03_xml.py

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module for serialization and deserialization using XML"""
+import xml.etree.ElementTree as ET
+
+
+def serialize_to_xml(dictionary, filename):
+    """Serializes a Python dictionary to an XML file"""
+    root = ET.Element("data")
+    for key, value in dictionary.items():
+        child = ET.SubElement(root, key)
+        child.text = str(value)
+    tree = ET.ElementTree(root)
+    tree.write(filename)
+
+
+def deserialize_from_xml(filename):
+    """Deserializes an XML file and returns a Python dictionary"""
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    return {child.tag: child.text for child in root}
